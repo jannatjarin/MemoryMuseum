4fbf0b3312435141dc981a43478270a21e6001bc
Jannat Jarin <jannatjarin3@gmail.com>
2026-07-21 23:38:37 +0600

    Created game entry point and initialized game class

---
+++ game.py
@@ -0,0 +1,50 @@
+import pygame
+
+from card import Card
+from painting import Painting
+
+
+class Game:
+
+    def __init__(self):
+
+        pygame.init()
+
+        self.width = 1000
+        self.height = 700
+        self.title = "Memory Museum"
+
+        self.background_color = (230, 225, 215)
+        self.screen = pygame.display.set_mode(
+            (self.width, self.height)
+        )
+
+        pygame.display.set_caption(self.title)
+
+        self.clock = pygame.time.Clock()
+        self.fps = 60
+        self.running = True
+        self.cards = []
+        self.selected_cards = []
+        self.matches = 0
+        self.attempts = 0
+        self.score = 0
+        self.painting = Painting()
+        
+        def initialize_cards(self):
+           self.cards = []
+
+        card_names = [
+            "A","A",
+            "B","B",
+            "C","C",
+            "D","D",
+            "E","E",
+            "F","F",
+            "G","G",
+            "H","H"
+        ]
+
+        for name in card_names:
+            card = Card(name)
+            self.cards.append(card)
