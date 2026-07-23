e0061c42ceb34a03002ca1916150a534219cecc3
anuanika21 <iamanuanika21@gmail.com>
2026-07-23 18:05:57 +0600

    added image,size,color on card

--- card.py
+++ card.py
@@ -2,20 +2,26 @@ import pygame
  
  
 class Card:
- 
-    def __init__(self, card_name, x, y, width, height):
- 
+
+    def __init__(self, card_name):
+
         self.card_name = card_name
- 
-        self.x = x
-        self.y = y
- 
-        self.width = width
-        self.height = height
- 
+
+        self.image = None
+
+        self.back_color = (110, 90, 70)
+
+        self.front_color = (240, 235, 220)
+
         self.is_flipped = False
+
         self.is_matched = False
- 
+
+        self.x = 0
+        self.y = 0
+
+        self.width = 120
+        self.height = 120
     def flip(self):
  
         self.is_flipped = True
@@ -38,7 +44,8 @@ class Card:
  
     def get_matched(self):
  
-        return self.is_matched
+        def set_image(self, image):
+            self.image = image
  
     def is_clicked(self, mouse):
  
@@ -61,85 +68,54 @@ class Card:
         return False
  
     def draw(self, screen):
- 
-        if self.is_flipped:
- 
-            color = (220, 220, 220)
- 
-        else:
- 
-            color = (110, 90, 70)
- 
-        pygame.draw.rect(
- 
-            screen,
- 
-            color,
- 
-            (
- 
-                self.x,
- 
-                self.y,
- 
-                self.width,
- 
-                self.height
- 
-            ),
- 
-            border_radius=8
- 
+
+        rectangle = pygame.Rect(
+            self.x,
+            self.y,
+            self.width,
+            self.height
         )
- 
-        pygame.draw.rect(
- 
-            screen,
- 
-            (0, 0, 0),
- 
-            (
- 
-                self.x,
- 
-                self.y,
- 
-                self.width,
- 
-                self.height
- 
-            ),
- 
-            2,
- 
-            border_radius=8
- 
-        )
- 
-        if self.is_flipped:
- 
-            font = pygame.font.SysFont(None, 40)
- 
-            text = font.render(
- 
-                self.card_name,
- 
-                True,
- 
-                (0, 0, 0)
- 
-            )
- 
-            text_rect = text.get_rect(
- 
-                center=(
- 
-                    self.x + self.width // 2,
- 
-                    self.y + self.height // 2
- 
+
+        if self.is_flipped or self.is_matched:
+
+            if self.image is not None:
+
+                screen.blit(
+                    self.image,
+                    rectangle
                 )
- 
+
+            else:
+
+                pygame.draw.rect(
+                    screen,
+                    self.front_color,
+                    rectangle,
+                    border_radius=8
+                )
+
+                font = pygame.font.SysFont(None, 40)
+
+                text = font.render(
+                    self.card_name,
+                    True,
+                    (50,50,50)
+                )
+
+                text_rect = text.get_rect(
+                    center=rectangle.center
+                )
+
+                screen.blit(
+                    text,
+                    text_rect
+                )
+
+        else:
+
+            pygame.draw.rect(
+                screen,
+                self.back_color,
+                rectangle,
+                border_radius=8
             )
- 
-            screen.blit(text, text_rect)
